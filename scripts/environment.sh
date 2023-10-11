a7b79ecb2c58ab277443adbbacbff384b652bb4c
AngelaCooljx <cool@gayhub.eu.org>
2023-10-11 20:18:52 +0800

    Delete package 'python-docutils'

--- scripts/environment.sh
+++ scripts/environment.sh
@@ -4,5 +4,5 @@ sudo apt install -y ack antlr3 asciidoc autoconf automake autopoint binutils bis
   libglib2.0-dev libgmp3-dev libltdl-dev libmpc-dev libmpfr-dev libncurses5-dev libncursesw5 \
   libncursesw5-dev libpython3-dev libreadline-dev libssl-dev libtool lld llvm lrzsz mkisofs msmtp \
   nano ninja-build p7zip p7zip-full patch pkgconf python2.7 python3 python3-pip python3-ply \
-  python-docutils python3-pyelftools qemu-utils re2c rsync scons squashfs-tools subversion swig \
+  python3-pyelftools qemu-utils re2c rsync scons squashfs-tools subversion swig \
   texinfo uglifyjs upx-ucl unzip vim wget xmlto xxd zlib1g-dev
